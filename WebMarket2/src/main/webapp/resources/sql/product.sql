d16ee39cefbcae5024b34f2fd8c60a431c69b6d2
admin <admin@DESKTOP-BSVD8GJ>
2023-03-17 14:04:13 +0900

    15장 DB 쿼리 웹마켓

--- WebMarket2/src/main/webapp/resources/sql/product.sql
+++ WebMarket2/src/main/webapp/resources/sql/product.sql
@@ -1,14 +1,14 @@
-CREATE TABLE IF NOT EXISTS product(
+CREATE TABLE  product(
 	p_id VARCHAR(10) NOT NULL,
 	p_name VARCHAR(20),
 	p_unitPrice  INTEGER,
-	p_description TEXT,
+	p_description VARCHAR(500),
    	p_category VARCHAR(20),
 	p_manufacturer VARCHAR(20),
 	p_unitsInStock LONG,
 	p_condition VARCHAR(20),
 	p_fileName  VARCHAR(20),
 	PRIMARY KEY (p_id)
-)default CHARSET=utf8;
+)default ;
 
 desc product;
